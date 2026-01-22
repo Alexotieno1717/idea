4c2e4482060aeec80c6d0a9944163dcf557a2a33
Alexotieno1717 <alexotieno900@gmail.com>
2026-01-22 12:59:18 +0300

    [feat] added test and created models, request,policy for idea

---
+++ tests/Unit/IdeaTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\Idea;
+use App\Models\User;
+use Illuminate\Support\Collection;
+
+test('it belongs to a user', function () {
+    $idea = Idea::factory()->create();
+
+    expect($idea->user)->toBeInstanceOf(User::class);
+});
+
+
+test('it have steps', function () {
+    $idea = Idea::factory()->create();
+
+    expect($idea->steps)->toBeEmpty();
+
+    $idea->steps()->create([
+        'description' => 'to be a thing',
+    ]);
+
+    expect($idea->fresh()->steps)->toHaveCount(1);
+});
